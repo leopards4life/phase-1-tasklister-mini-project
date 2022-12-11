c9b2d4fbc8432d147c044898619ad8b8b7f4aa42
Jessica Vaughn <jessica.vaughn619@gmail.com>
2022-12-10 17:23:22 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let submitButton = document.getElementById("create-task-form");
+  submitButton.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  let list = document.getElementById("new-task-description").submit();
+  event.preventDefault();
+}
 });
+
