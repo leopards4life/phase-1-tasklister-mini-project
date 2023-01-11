95038a37403e079e1fe8cf6d184559d864022b26
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-10 18:08:41 -0700

    solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
   submitButton.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-  let list = document.getElementById("new-task-description").submit();
+  let taskItem = document.getElementById("new-task-description").value;
   event.preventDefault();
+  let todoList = document.getElementById("tasks");
+  let li = document.createElement("li");
+  li.innerHTML = taskItem;
+  todoList.appendChild(li);
 }
 });
 
+// As a user, I should be able to type a task into the input field.
+// As a user, I should be able to click some form of a submit button.
+// As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated. 
